117cb01bb6d7726171e05d9a802c28d9e3174eb3
Samuel de Mello Cagnani <103051866+SamuelCagnani@users.noreply.github.com>
2023-08-09 10:46:55 -0300

    Add files via upload

---
+++ ex6.1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+
+
+    /* Exercício 6.1: Escreva um programa que peça para o usuário fornecer os coeficientes
+    * a, b e c de um polinômio do segundo grau. O programa deve calcular as duas raízes da
+    * equação do segundo grau representada por esse polinômio e apresentar o conjunto
+    * solução (S = {x1,x2}) ao usuário, sendo que os valores de x devem ser apresentados
+    * em ordem crescente. Caso o coeficiente a seja igual a zero, significa que não existe
+    * equação do segundo grau, então uma mensagem deve ser exibida ao usuário e o
+    * programa deve finalizar. Caso o discriminante da equação (∆) seja menor que zero,
+    * não existem raízes reais, sendo assim, o conjunto solução é vazio. Caso seja igual a zero,
+    * as duas raízes têm o mesmo valor e apenas uma deve ser apresentada no conjunto
+    * solução. Caso seja maior que zero, existem duas raízes reais distintas que devem ser
+    * apresentadas no conjunto solução, em ordem crescente. Apresente também o valor
+    * de ∆. Todos os valores são decimais e devem ser apresentados usando duas casas de
+    * precisão.
+    */
+
+
+
+
+    return 0;
+
+}
